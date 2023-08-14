a5d487a23fa9f3932f5e62c3201cd10dbfee361f
OliwierJaworski <114076196+OliwierJaworski@users.noreply.github.com>
2023-08-15 00:42:29 +0200

    php rework

--- PHPTEST1.php
+++ PHPTEST1.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-<title>My Page</title>
-<body>
-
-<h1>Welcome to my page</h1>
-
 <?php
 $host = "127.0.0.1";
 $port = "5432";
@@ -17,11 +8,9 @@ $password = "OliExam";
 // Establish a connection
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . pg_last_error());
+if ($conn) {
+    echo "Connected to the database successfully!";
+} else {
+    echo "Connection failed.";
 }
-echo "Connected successfully";
 ?>
-</body>
-</html>
