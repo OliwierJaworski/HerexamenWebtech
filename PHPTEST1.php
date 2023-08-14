51012141833294cfce0c00b5af31cf8df8c82751
OliwierJaworski <114076196+OliwierJaworski@users.noreply.github.com>
2023-08-14 23:45:51 +0200

    first try php request

---
+++ PHPTEST1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello, World!</title>
+</head>
+<body>
+<h1>Hello, World!</h1>
+<p>This is a simple HTML page.</p>
+
+<?php
+$connection = pg_connect("host=127.0.0.0")
+?>
+</body>
+</html>
