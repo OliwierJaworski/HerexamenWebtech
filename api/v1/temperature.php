52395dc6ba104f5dbc55cc8f7e9f3a11eaf8fb16
OliwierJaworski <114076196+OliwierJaworski@users.noreply.github.com>
2023-08-16 15:25:28 +0200

    added debug part

--- api/v1/temperature.php
+++ api/v1/temperature.php
@@ -1,3 +1,7 @@
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 <?php
 // Get the JSON data from the incoming request
 $body = file_get_contents("php://input");
