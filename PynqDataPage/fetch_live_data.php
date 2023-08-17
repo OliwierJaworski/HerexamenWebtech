59d8aca2f8fe2fe247037f15743a86dbfce71b94
OliwierJaworski <114076196+OliwierJaworski@users.noreply.github.com>
2023-08-17 03:39:04 +0200

    file responsible for generating json adjusted

--- PynqDataPage/fetch_live_data.php
+++ PynqDataPage/fetch_live_data.php
@@ -1,4 +1,3 @@
-<?php
 $host = '127.0.0.1';
 $port = 5432;
 $dbname = 'pynqdb';
@@ -8,23 +7,25 @@ $password = 'oli';
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
 if (!$conn) {
-    die("Connection failed: " . pg_last_error());
+die("Connection failed: " . pg_last_error());
 }
 
-$query = "SELECT temperature FROM SendData"; // Update with your table name
+$query = "SELECT temperature, time FROM SendData"; // Include the 'time' column
 $result = pg_query($conn, $query);
 
 if (!$result) {
-    die("Query failed: " . pg_last_error());
+die("Query failed: " . pg_last_error());
 }
 
 $data = array();
 while ($row = pg_fetch_assoc($result)) {
-    $temperature[] = $row['temperature'];
+$data[] = array(
+"temperature_column" => $row['temperature'],
+"date_column" => $row['time']
+);
 }
 
 pg_close($conn);
 
 header('Content-Type: application/json');
 echo json_encode($data);
-?>
