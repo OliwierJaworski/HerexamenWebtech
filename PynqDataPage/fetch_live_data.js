2965144a415ca3afbf8a19d1c5ea49d7fb18d1f6
OliwierJaworski <114076196+OliwierJaworski@users.noreply.github.com>
2023-08-17 16:05:31 +0200

    graph inladen adhv js

--- PynqDataPage/fetch_live_data.js
+++ PynqDataPage/fetch_live_data.js
@@ -1,37 +1,47 @@
-$(document).ready(function() {
-    $('#fetchDataForm').submit(function(event) {
-        event.preventDefault();
+const { Pool } = require('pg');
+const Chart = require('chart.js');
 
-        $.ajax({
-            url: 'fetch_graph_data.php',
-            method: 'POST',
-            dataType: 'json',
-            success: function(data) {
-                const labels = data.map(entry => entry.time);
-                const values = data.map(entry => entry.temperature);
-
-                const ctx = document.getElementById('myChart').getContext('2d');
-                const myChart = new Chart(ctx, {
-                    type: 'bar',
-                    data: {
-                        labels: labels,
-                        datasets: [{
-                            label: 'Temperature',
-                            data: values,
-                            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                            borderColor: 'rgba(75, 192, 192, 1)',
-                            borderWidth: 1
-                        }]
-                    },
-                    options: {
-                        scales: {
-                            y: {
-                                beginAtZero: true
-                            }
-                        }
-                    }
-                });
-            }
-        });
-    });
+// PostgreSQL connection settings
+const pool = new Pool({
+    user: 'postgres',
+    host: '127.0.0.1',
+    database: 'pynqdb',
+    password: 'oli',
+    port: 5432,
 });
+
+// Fetch data from the database
+async function fetchData() {
+    try {
+        const result = await pool.query('SELECT time, temperature FROM SendData ORDER BY time DESC LIMIT 20');
+        return result.rows;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return [];
+    }
+}
+
+// Create the graph
+async function createGraph() {
+    const data = await fetchData();
+
+    const labels = data.map(entry => entry.time);
+    const values = data.map(entry => entry.temperature);
+
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Temperature',
+                data: values,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+                fill: false,
+            }],
+        },
+    });
+}
+
+createGraph();
