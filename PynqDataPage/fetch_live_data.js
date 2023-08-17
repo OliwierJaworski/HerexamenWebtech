d555be3d5524163b4cb613bf13703782d1c511df
OliwierJaworski <114076196+OliwierJaworski@users.noreply.github.com>
2023-08-17 22:00:09 +0200

    testing xhr2 for Httprequest

--- PynqDataPage/fetch_live_data.js
+++ PynqDataPage/fetch_live_data.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", fetchData);
 
 function fetchData() {
     alert("fetching data...");
+    var XMLHttpRequest = require('xhr2');
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://server-of-oliwier.pxl.bjth.xyz/PynqDataPage/server.js%22);
     xhr.onreadystatechange = readyChange;
