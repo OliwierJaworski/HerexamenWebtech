11a7f5fd7263e32595c1820102c4d96ea0d090ac
OliwierJaworski <114076196+OliwierJaworski@users.noreply.github.com>
2023-08-18 03:25:35 +0200

    adjusted ajaxofindex

---
+++ PynqDataPage/ajaxofindex.js
@@ -0,0 +1,24 @@
+function fetchData(callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/data");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText);
+                callback(data);
+            } else {
+                console.error("Error fetching data");
+            }
+        }
+    };
+    xhr.send();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    function displayData(data) {
+        var container = document.getElementById("data-container");
+        container.textContent = JSON.stringify(data, null, 2);
+    }
+
+    fetchData(displayData);
+});
