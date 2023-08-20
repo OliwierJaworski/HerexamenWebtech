9506c09b74cbce9e02623e164c5783de4fdbb15d
OliwierJaworski <114076196+OliwierJaworski@users.noreply.github.com>
2023-08-20 16:10:00 +0200

    testing if script actually reloads page

--- PynqDataPage/ajaxofindex.js
+++ PynqDataPage/ajaxofindex.js
@@ -1,7 +1,5 @@
-$(document).ready(function()
-{
-    function displayData(data)
-    {
+$(document).ready(function() {
+    function displayData(data) {
         var container = document.getElementById("data-container");
         var html = "<h2>Last 20 Temperature Readings</h2>";
         html += "<table><tr><th>Time</th><th>Temperature</th></tr>";
@@ -17,10 +15,12 @@ $(document).ready(function()
         container.innerHTML = html;
     }
 
-    function updateData()
-    {
+    function updateData() {
         console.log("Updating data...");
         fetchData(displayData);
+
+        var square = document.getElementById("color-square");
+        square.style.backgroundColor = square.style.backgroundColor === "white" ? "black" : "white";
     }
 
     function fetchData(callback) {
@@ -28,60 +28,13 @@ $(document).ready(function()
             if (status === "success") {
                 var data = JSON.parse(response);
                 callback(data);
-            }
-            else
-            {
+            } else {
                 console.error("Error fetching data");
             }
         });
     }
 
     setInterval(updateData, 5000);
-
-
-}
-/*function fetchData(callback)
-   {
-   var xhr = new XMLHttpRequest();
-   xhr.open("GET", "https://server-of-oliwier.pxl.bjth.xyz/PynqDataPage/fetch_live_data.php");
-   xhr.onreadystatechange = function() {
-       if (xhr.readyState === XMLHttpRequest.DONE)
-       {
-           if (xhr.status === 200)
-           {
-               var data = JSON.parse(xhr.responseText); // Parse the JSON response
-               callback(data);
-           } else
-           {
-               console.error("Error fetching data");
-           }
-       }
-   };
-   xhr.send();
-   }
-   */
-
-/*document.addEventListener("DOMContentLoaded", function()
-{
-    function displayData(data)
-    {
-        var container = document.getElementById("data-container");
-        var html = "<h2>Last 20 Temperature Readings</h2>";
-        html += "<table><tr><th>Time</th><th>Temperature</th></tr>";
-
-        // Loop through each data object and format it
-        for (var i = 0; i < data.length; i++) {
-            var time = data[i].time; // Time in JSON format
-            var temperature = data[i].temperature;
-            html += "<tr><td>" + time + "</td><td>" + temperature + "</td></tr>";
-        }
-
-        html += "</table>";
-        container.innerHTML = html;
-    }
-
-    //fetchData(displayData);
-    setInterval(updateData, 5000);
-    */
-
 });
+
+
