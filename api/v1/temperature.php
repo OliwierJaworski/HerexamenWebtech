364eb132b0966f62b57939abc6e6c95d687629de
OliwierJaworski <114076196+OliwierJaworski@users.noreply.github.com>
2023-08-20 12:33:41 +0200

    sql injections resolved for temperature.php

--- api/v1/temperature.php
+++ api/v1/temperature.php
@@ -3,7 +3,6 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 <?php
-// Get the JSON data from the incoming request
 $body = file_get_contents("php://input");
 echo "Received data: $body<br>";
 $data = json_decode($body, true);
@@ -12,41 +11,35 @@ if (!$data) {
     die("Error decoding JSON data.");
 }
 
-// Extract the temperature value from the data
 $temperature = isset($data['temperature']) ? $data['temperature'] : null;
 
 if ($temperature === null) {
     die("Temperature data missing in JSON.");
 }
 
-// Generate the current timestamp
 $currentTimestamp = date('Y-m-d H:i:s');
 
-// Connect to the PostgreSQL database
 $host = '127.0.0.1';
 $port = 5432;
 $dbname = 'pynqdb';
 $user = 'postgres';
-$password = 'oli';
+$password = 'HeelMoeilijkeWachtWoordVoorDatabase';
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
-
-if (!$conn) {
+if (!$conn)
+{
     die("Connection failed: " . pg_last_error());
 }
 
-// Insert temperature and current timestamp data into the database
-$query = "INSERT INTO SendData (temperature, time) VALUES ($temperature, '$currentTimestamp')";
-$result = pg_query($conn, $query);
-
-if (!$result) {
+$query = "INSERT INTO SendData (temperature, time) VALUES ($1, $2)";
+$result = pg_query_params($conn, $query, array($temperature, $currentTimestamp));
+if (!$result)
+{
     die("Insert failed: " . pg_last_error());
 }
 
-// Close the database connection
 pg_close($conn);
 
-// Send a response back to the PYNQ device
 $response = array("message" => "Temperature data inserted successfully");
 echo json_encode($response);
 ?>
